b7e4e2f6f1c451a4890bd2ed1f95d8f43c185043
SaadInHack <muslimdasturchi@gmail.com>
2024-07-18 20:47:46 +0500

    Some changes are made in main

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ from gui import ToDoListGUI
 import tkinter as tk
 
 def main():
-    if len(sys.argv) > 1 and sys.argv[1] == "cli":
-        cli_app = ToDoListCLI()
-        cli_app.run()
-    else:
-        root = tk.Tk()
-        gui_app = ToDoListGUI(root)
-        root.mainloop()
+    while True:
+        check = int(input("Enter the number of command:\n1.GUI\n2.CLI\n0.Exit\n"))
+        if check == 2:
+            cli_app = ToDoListCLI()
+            cli_app.run()
+        elif check == 1:
+            root = tk.Tk()
+            gui_app = ToDoListGUI(root)
+            root.mainloop()
+        elif check == 0:
+            break;
+        else:
+            print("Unknown command. Please try again.")
 
 if __name__ == "__main__":
     main()
